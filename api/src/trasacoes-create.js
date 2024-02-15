b263d3356291c8a19348e210a0d2e5417fbe690f
Lucas Silva <lucashenrique580@gmail.com>
2024-02-15 11:56:39 -0300

    cria transaction junto da regra de negócio e melhora validação do input.

--- api/src/trasacoes-create.js
+++ api/src/trasacoes-create.js
@@ -1,48 +1,62 @@
 module.exports = {
-  run: async (sql, { clientId, valor, tipo, descricao }) => {
-    Number(1.2*100).toFixed(2)
-    valor = valor*100
+  run: async function (sql, { clientId, valor, tipo, descricao }) {
+    // return await sql.begin('TRANSACTION ISOLATION LEVEL REPEATABLE READ', async transaction => {
+    // return await sql.begin('TRANSACTION ISOLATION LEVEL SERIALIZABLE', async transaction => {
+    return await sql.begin('', async transaction => { // default é READ COMMITED
+      if (!Number.isInteger(clientId)) {
+        throw new Error('invalid_client_id');
+      }
 
-    if (!['c', 'd'].includes(tipo)) {
-      throw new Error('invalid_transaction_type');
-    }
-    if (descricao.length > 10) {
-      throw new Error('description_too_long');
-    }
+      if (!Number.isInteger(valor) || valor < 0) {
+        throw new Error('invalid_transaction_value');
+      }
 
-    const [clientData,] = await sql`
-      SELECT c.limite AS total_limit, s.valor AS balance, s.id AS balance_id
-      FROM clientes c
-      JOIN saldos s ON s.cliente_id=c.id
-      WHERE c.id=${clientId}
-      FOR UPDATE;
-    `;
+      if (!['c', 'd'].includes(tipo)) {
+        throw new Error('invalid_transaction_type');
+      }
 
-    if (!clientData) {
-      throw new Error('client_not_found');
-    }
+      if (!descricao || descricao === '' || descricao.length > 10) {
+        throw new Error('invalid_description');
+      }
 
-    const { total_limit, balance, balance_id } = clientData;
-    const newBalance = tipo === 'c' ? balance + valor : balance - valor;
+      const [clientData,] = await transaction`
+        SELECT c.limite AS total_limit, s.valor AS balance, s.id AS balance_id
+        FROM clientes c
+        JOIN saldos s ON s.cliente_id=c.id
+        WHERE c.id=${clientId}
+        FOR UPDATE;
+      `;
 
-    if (Math.abs(newBalance) > total_limit) {
-      throw new Error('insufficient_limit');
-    }
+      if (!clientData) {
+        throw new Error('client_not_found');
+      }
 
-    const updateBalanceQuery = sql`
-      UPDATE saldos AS s SET valor=${newBalance} WHERE s.id=${balance_id};
-    `;
-    const insertTransactionQuery = sql`
-      INSERT INTO transacoes
-      (cliente_id, valor, tipo, descricao, realizada_em)
-      VALUES (${clientId}, ${valor}, ${tipo}, ${descricao}, ${new Date().toISOString()});
-    `;
+      const { total_limit, balance, balance_id } = clientData;
+      const newBalance = tipo === 'c' ? balance + valor : balance - valor;
 
-    await Promise.all([updateBalanceQuery, insertTransactionQuery]);
+      if (tipo === 'd' && Math.abs(newBalance) > total_limit) {
+        throw new Error('insufficient_limit');
+      }
 
-    return {
-      limite: total_limit,
-      saldo: newBalance
-    }
+      const updateBalanceQuery = transaction`
+        UPDATE saldos AS s SET valor=${newBalance} WHERE s.id=${balance_id};
+      `;
+
+      const insertTransactionQuery = transaction`
+        INSERT INTO transacoes
+        (cliente_id, valor, tipo, descricao, realizada_em)
+        VALUES (${clientId}, ${valor}, ${tipo}, ${descricao}, ${new Date().toISOString()});
+      `;
+
+      await Promise.all([updateBalanceQuery, insertTransactionQuery]);
+
+      return {
+        limite: total_limit,
+        saldo: newBalance
+      }
+    }).catch(async err => {
+
+      throw err;
+    });
   }
 }
